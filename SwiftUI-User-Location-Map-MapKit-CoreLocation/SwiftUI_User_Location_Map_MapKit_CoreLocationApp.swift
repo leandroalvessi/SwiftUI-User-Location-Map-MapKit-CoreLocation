45a0df2875b101da32b46fced19900a83d1a4c65
Leandro Alves da Silva <leandroalves.desenvolvedor@gmail.com>
2022-06-14 16:19:58 -0300

    Initial Commit

---
+++ SwiftUI-User-Location-Map-MapKit-CoreLocation/SwiftUI_User_Location_Map_MapKit_CoreLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_User_Location_Map_MapKit_CoreLocationApp.swift
+//  SwiftUI-User-Location-Map-MapKit-CoreLocation
+//
+//  Created by Leandro Alves da Silva on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_User_Location_Map_MapKit_CoreLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
